67e5a428960c6ba924f44b2ea90cfd597da11194
HamBrick327 <92757936+HamBrick327@users.noreply.github.com>
2022-11-01 22:14:50 -0600

    updated main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import cv2
 
+scuffedMap = 0.10980392156862745098039215686275
+
 # define the video capture 
 vid = cv2.VideoCapture(0)
 
 # define an object containing an image
-img = cv2.imread('rick-astley.png') # 0 as the second argument loads the image as grayscale
+img = cv2.imread('rick-astley.png', 0) # 0 as the second argument loads the image as grayscale
 
 # create log object
 log = open('log.txt', 'w')
@@ -19,31 +21,11 @@ density = 'Ñ@#W$9876543210?!abc;:+=-,._ '
 cv2.imshow("image", img)
 
 # loop through all pixels in the image and print the rgb value
-
-'''
 for i in range(img.shape[0]-1):
     for j in range(img.shape[1]-1):
         print(i, j, end='\r')
         log.write(str(i) + ' ' + str(j) + '\n')
-'''
-for i in range(255):
-    print(i*0.10980392156862745098039215686275)
 
 
-while False:
-
-    # capture video frame by frame
-    frame = vid.read()[1] # vid.read()[1] only reads the array of the single frame, there is another attribute that comes before it in the video
-    print(frame)
-
-    # display captured video
-    cv2.imshow("frame", frame) # frame object is updated every recursion; doesn't make new window, only updates existing window
-
-    # q set at close keybind
-    if cv2.waitKey(1) & 0xFF == ord('q'): ## if statement doesn't like 'and'; must use '&'
-        break
-
-# release video capture object
-vid.release()
 # kill all windows
 cv2.destroyAllWindows()
